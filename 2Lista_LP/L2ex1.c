44e660ad02c9fe8a26ed5a39ac683c04cc8d11ee
jonathas.moraes <jonathas.moraes@etec.sp.gov.br>
2018-09-22 11:46:31 -0400

    segunda lista de LP

---
+++ 2Lista_LP/L2ex1.c
@@ -0,0 +1,13 @@
+//Dada uma sequência de n números, imprimí-la na ordem invera da leituira//
+
+#include<stdio.h>
+
+int main(){
+	int s, n, a;
+	
+	scanf("%d", &s);
+	
+	for(n=0; n<s; n++){
+		
+	}
+}
